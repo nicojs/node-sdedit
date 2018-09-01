eaa3822175e814f0bb17578ec915c7f914af1ba5
Nico Jansen <jansennico@gmail.com>
2018-09-02 01:09:03 +0200

    fix(deps): update dependencies

--- test/helpers/mock.ts
+++
@@ -1,9 +0,0 @@
-import * as sinon from 'sinon';
-
-export type Mock<T> = {
-    [K in keyof T]: sinon.SinonStub;
-};
-
-export function mock<T>(constructor: { new(...args: any[]): T }): Mock<T> {
-    return sinon.createStubInstance(constructor);
-}
